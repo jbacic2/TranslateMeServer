e4d12292d7a13c969176a8a8ef1b4fb132e4bb65
Omar Flores <omar.garciaf247@gmail.com>
2019-01-27 00:26:47 -0500

    Server update

--- client.js
+++ client.js
@@ -1,23 +1,20 @@
 let socket = io('http://' + window.document.location.host);
 
-console.log(socket);
+//console.log(socket);
 
 console.log("hi im here")
 
-socket.on('connection', () => {
-    console.log("hi");
 
-    let newUser = {
-        username: 'omar',
-        name: 'hi',
-        role: 'OFFLINE',
-        longitude: 12,
-        latitude: 14,
-        socketID: null,
-        matchedWith: null
-    }
+console.log("hi");
 
-    socket.emit('logon', newUser);
+let newUser = {
+    username: 'omar',
+    name: 'hi',
+    role: 'OFFLINE',
+    longitude: 12,
+    latitude: 14,
+    socketID: null,
+    matchedWith: null
+}
 
-})
-
+socket.emit('logon', newUser);
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const io = require('socket.io')(server);
 
 app.use(express.json())
 
-var appUsers =[];
+var appUsers = [];
 
 //express.static('/');
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
-app.post("/register", function(req,res){
+app.post("/register", function (req, res) {
     newUser = {
         username: req.body.username,
         name: req.body.name,
@@ -22,27 +22,40 @@ app.post("/register", function(req,res){
         socketID: null,
         matchedWith: null
     }
-  
-   appUsers.push(newUser);
-   res.send(JSON.stringify({approved:true}))
+
+    appUsers.push(newUser);
+    res.send(JSON.stringify({
+        approved: true
+    }))
 })
 
 //sockets    
-io.on('connection', (socket) => { 
+io.on('connection', (socket) => {
+    socket.emit("logon",
+        newUser = {
+            username: 'omar',
+            name: 'hi',
+            role: 'OFFLINE',
+            longitude: 12,
+            latitude: 14,
+            socketID: null,
+            matchedWith: null
+        })
+
     let thisSocketID = socket.id;
     let currentPerson;
 
     //currentPerson.socketID = socket.id;
     socket.on("logon", (data) => {
-        let temp = JSON.parse(data);
+        let temp = data
 
         console.log(temp);
 
-        for (person of appUsers){
+        for (person of appUsers) {
             if (person.username == temp.username)
                 currentPerson = person;
         }
-    
+
         currentPerson.socketID = thisSocketID;
 
         currentPerson.longitude = temp.longitude;
@@ -53,7 +66,7 @@ io.on('connection', (socket) => {
     })
 
     //let currentPerson;
-    
+
     //console.log("This is the socket", socket)
 
     //find user
@@ -65,7 +78,7 @@ io.on('connection', (socket) => {
     // currentPerson.longitude = req.body.longitude;
     // currentPerson.latitude = req.body.latitude;
     // currentPerson.role = req.body.role;
-        
+
 
     //lookForMatch(currentPerson);
 
@@ -74,126 +87,125 @@ io.on('connection', (socket) => {
 
         send.res("");
     });*/
-    
+
 
     //updates location
-    socket.on("locationUpdate", function(from, data){
+    socket.on("locationUpdate", function (from, data) {
         currentPerson.longitude = req.body.longitude;
         currentPerson.latitude = req.body.latitude;
 
-        if (currentPerson.matched == true){
+        if (currentPerson.matched == true) {
             mapUpdate(currentPerson);
-        }
-        else{
+        } else {
             lookForMatch(currentPerson);
         }
 
     });
-    
-});//io.on connect
+
+}); //io.on connect
 
 
 
 
-server.listen(process.env.PORT || 8080);//************ */
+server.listen(process.env.PORT || 8080); //************ */
 
-  //*********************************************** 
-  //********************************************* */
-  //********************************************* */
-  //********************************************* */
+//*********************************************** 
+//********************************************* */
+//********************************************* */
+//********************************************* */
 
-  function lookForMatch(currentPerson){
+function lookForMatch(currentPerson) {
 
     //check for match USER
-    if (currentPerson.role == USER){
+    if (currentPerson.role == USER) {
         var currentTranslators = [];
 
         //find list of tranlators
-        for (let i = 0; i<appUsers.length; i++){
+        for (let i = 0; i < appUsers.length; i++) {
             if (appUsers[i].role == "TRANSLATOR")
                 currentTranslators.push(appUsers[i]);
         }
 
         //if there are multiple translators find the closest
-        if (currentTranslators.length>0){
-            var nearestTranslator = currentTranslators[0]; 
+        if (currentTranslators.length > 0) {
+            var nearestTranslator = currentTranslators[0];
 
-            for (translator in currentTranslators){
-                if (findDist(currentPerson.location, nearestTranslator.location)> findDist(currentPerson.location, translator.location))
+            for (translator in currentTranslators) {
+                if (findDist(currentPerson.location, nearestTranslator.location) > findDist(currentPerson.location, translator.location))
                     nearestTranslator = translator;
             }
 
             match(currentPerson, nearestTranslator);
 
-        }//if 
+        } //if 
     }
 
-    if (currentPerson.role == TRANSLATOR){
+    if (currentPerson.role == TRANSLATOR) {
         var currentUsers = [];
-        
-         //find list of users
-         for (let i = 0; i<appUsers.length; i++){
+
+        //find list of users
+        for (let i = 0; i < appUsers.length; i++) {
             if (appUsers[i].role == "USER")
                 currentUsers.push(appUsers[i]);
         }
 
 
-        if (currentUsers.length>0){
-            var nearestUser = currentUsers[0]; 
+        if (currentUsers.length > 0) {
+            var nearestUser = currentUsers[0];
 
-            for (user in currentUsers){
-                if (findDist(currentPerson.location, nearestUser.location)> findDist(currentPerson.location, user.location))
+            for (user in currentUsers) {
+                if (findDist(currentPerson.location, nearestUser.location) > findDist(currentPerson.location, user.location))
                     nearestUser = user
             }
 
             match(nearestUser, currentPerson);
 
-        }//if 
+        } //if 
     }
-  }//lookForMatch
+} //lookForMatch
 
-  function findDist(person1, person2){
+function findDist(person1, person2) {
     var earthRadius = 3958.75;
-    var latDiff = Math.toRadians(person1.latitude-person2.latitude);
-    var lngDiff = Math.toRadians(person1.longitude-person2.longitude);
-    var a = Math.sin(latDiff /2) * Math.sin(latDiff /2) + Math.cos(Math.toRadians(person2.latitude)) * Math.cos(Math.toRadians(person1.latitude)) * Math.sin(lngDiff /2) * Math.sin(lngDiff /2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var latDiff = Math.toRadians(person1.latitude - person2.latitude);
+    var lngDiff = Math.toRadians(person1.longitude - person2.longitude);
+    var a = Math.sin(latDiff / 2) * Math.sin(latDiff / 2) + Math.cos(Math.toRadians(person2.latitude)) * Math.cos(Math.toRadians(person1.latitude)) * Math.sin(lngDiff / 2) * Math.sin(lngDiff / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var distance = earthRadius * c;
 
     var meterConversion = 1609;
 
     return distance * meterConversion;
-  }
+}
 
-  function match(aUser, aTranslator){
-      aUser.matchedWith = aTranslator.username;
-      aTranslator.matchedWith =aUser.username;
+function match(aUser, aTranslator) {
+    aUser.matchedWith = aTranslator.username;
+    aTranslator.matchedWith = aUser.username;
 
-      //calculate path with api
-      https.get(`https://api.mapbox.com/directions/v5/mapbox/driving/${aTranslator.longitude},${aTranslator.latitude};${aUser.longitude},${aUser.latitude}.json?access_token=${mapKey}&overview=full`, (resp) => {
+    //calculate path with api
+    https.get(`https://api.mapbox.com/directions/v5/mapbox/driving/${aTranslator.longitude},${aTranslator.latitude};${aUser.longitude},${aUser.latitude}.json?access_token=${mapKey}&overview=full`, (resp) => {
         let data = ''
-     
+
         resp.on('data', (chunk) => {
-          data += chunk
+            data += chunk
         })
-     
+
         resp.on('end', () => {
-          let locDataObj = JSON.parse(data);
-          locDataObj.routes[0].geometry
+            let locDataObj = JSON.parse(data);
+            locDataObj.routes[0].geometry
         })
-      })//https get request
-     
-      
+    }) //https get request
 
-      var locTranslator = JSON.stringify(aTranslator.location);
-      var locUser = JSON.stringify(aUser.location);
-      io.to(`${aUser.socketID}`).emit('foundMatch', locTranslator);
-      io.to(`${aTranslator.socketID}`).emit('foundMatch', locUser);
-  }
 
-  function mapUpdate(currentPerson){
+
+    var locTranslator = JSON.stringify(aTranslator.location);
+    var locUser = JSON.stringify(aUser.location);
+    io.to(`${aUser.socketID}`).emit('foundMatch', locTranslator);
+    io.to(`${aTranslator.socketID}`).emit('foundMatch', locUser);
+}
+
+function mapUpdate(currentPerson) {
     //will send user data to other person
     var locData = currentPerson.matchedWith.location;
     io.to(`${currentPerson.matchedWith.socketID}`).emit('foundMatch', locData);
 
-  }
+}
